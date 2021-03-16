a36bc64403be80dab91e38daed45f4d165963d99
pribytkov <pribytkov@proit.ru>
2021-03-16 18:35:48 +0500

    Fix compare

--- src/main/scala/tree/MainTree.scala
+++ src/main/scala/tree/MainTree.scala
@@ -4,17 +4,26 @@ import scala.annotation.tailrec
 
 object MainTree extends App {
 
-//  @tailrec
   def compare(a: TreeNode[Int], b: TreeNode[Int]): Boolean = {
-    if (a != b) {
-      false
-    } else {
-      if (a.left.isDefined && b.left.isDefined && a.right.isDefined && b.right.isDefined) {
-        if (compare(a.left.get, a.right.get))
-          compare(b.left.get, b.right.get) else false
-      } else true
+    (a, b) match {
+      case (TreeNode(aValue, Some(aLeft), Some(aRight)), TreeNode(bValue, Some(bLeft), Some(bRight))) if aValue == bValue => {
+        compare(aLeft, bLeft) && compare(aRight, bRight)
+      }
+      case (TreeNode(aValue, None, None), TreeNode(bValue, None, None)) => aValue == bValue
+      case _ => false
     }
   }
+//  @tailrec
+//  def compare(a: TreeNode[Int], b: TreeNode[Int]): Boolean = {
+//    if (a != b) {
+//      false
+//    } else {
+//      if (a.left.isDefined && b.left.isDefined && a.right.isDefined && b.right.isDefined) {
+//        if (compare(a.left.get, a.right.get))
+//          compare(b.left.get, b.right.get) else false
+//      } else true
+//    }
+//  }
 }
 
 case class TreeNode[X](value: X, left: Option[TreeNode[X]], right: Option[TreeNode[X]])
