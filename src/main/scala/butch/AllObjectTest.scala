480908b455382c85b73b9381b4444a6a1c5d12a1
pribytkov <pribytkov@proit.ru>
2021-03-25 19:28:39 +0500

    add tacks

--- src/main/scala/butch/AllObjectTest.scala
+++ src/main/scala/butch/AllObjectTest.scala
@@ -1,24 +1,87 @@
 package butch
 
+import scala.+:
+import scala.annotation.tailrec
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 
 object AllObjectTest extends App {
-  /***
-   * Считаем колличество смен знаков у чисел
-   */
-  val list = List(45, -45, 67, 0, 54, 43, -35, -45, 0, 0 , 0, 1, 0, 1, -1, 2, -1, 0 , 0, -1, 9, -1)
+  val list: Seq[Int] = List(45, -45, 67, 0, 54, 43, -35, -45, 0, 0, 0, 1, 0, 1, -1, 2, -1, 0, 0, -1, 9, -1)
 
-  val resCountWithZero = list.foldLeft(0)((m, n) => {
-    if (m == 0) {
-      n.sign
-    } else if (m.sign != n.sign && (n != 0)) {
-      n.sign * (Math.abs(m) + 1)
-    }  else {
-      m
+  /** *
+   * Считаем колличество смен знаков у чисел в листе
+   */
+  def replaceSignsCount(list: Seq[Int]) = {
+    val resCountWithZero = list.foldLeft(0)((m, n) => {
+      if (m == 0) n.sign
+      else if (m.sign != n.sign && (n != 0)) n.sign * (Math.abs(m) + 1)
+      else m
+    })
+    Math.abs(resCountWithZero) - 1
+  }
+
+  /** *
+   * Ищем последний в листе
+   */
+  @tailrec
+  def lastElem(list: Seq[Int]): Int = list match {
+    case h :: Nil => h
+    case _ :: tail => lastElem(tail)
+  }
+
+  /** *
+   * Ищем ппредпоследнийм в листе
+   */
+
+  @tailrec
+  def preLastElem(list: Seq[Int]): Int = list match {
+    case h :: _ :: Nil => h
+    case _ :: tail => preLastElem(tail)
+  }
+
+  /** *
+   * Ищем k-й елемент в листе
+   */
+
+  @tailrec
+  def kElem(k: Int, list: Seq[Int]): Int = (k, list) match {
+    case (0, h :: _) => h
+    case (n, _ :: tail) => kElem(n, tail)
+  }
+
+  /** *
+   * Ищем k-й елемент в листе
+   */
+
+  @tailrec
+  def length(count: Int, list: Seq[Int]): Int = (count, list) match {
+    case (0, Nil) => 0
+    case (n, _ :: Nil) => n + 1
+    case (n, _ :: tail) => length(n + 1, tail)
+  }
+
+  /** *
+   * Разворачиваем k-й елемент в листе
+   */
+  def revertList(list: Seq[Int]): List[Int] = list match {
+    case h :: tail => {
+      revertList(tail) ::: List(h)
     }
-  })
-  val resCount = Math.abs(resCountWithZero) - 1
-  println(resCount)
+    case Nil => Nil
+  }
+
+  /***
+   * Замените серии одинаковых элементов списка на одиночный элемент
+ */
+  def replaceManyToOne(list: Seq[String]): List[String] =
+    list.foldRight(List[String]()) { (right, acc) => {
+      if (acc.isEmpty || acc.head != right) right :: acc else acc
+  }
+  }
+
+  val listStr = List("a", "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e", "e")
+
+  println("list.length: " + replaceManyToOne(listStr).mkString("; "))
+
 }
