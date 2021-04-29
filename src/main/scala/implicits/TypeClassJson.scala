e840355df2a3ea4659629aac23b6edfdd344d261
pribytkov <pribytkov@proit.ru>
2021-04-29 22:14:14 +0500

    ix according by task

--- src/main/scala/implicits/TypeClassJson.scala
+++ src/main/scala/implicits/TypeClassJson.scala
@@ -19,15 +19,16 @@ object TypeClassJson extends App {
 
   implicit object JsonObjectConverter extends ConverterType[JsonObject] {
     override def convert(obj: JsonObject): String = {
-      obj.map.map {case (key, value) => {
+      val jsonResult = obj.map.map {case (key, value) => {
         val convertValue: String = value match {
           case arrayObj: JsonArray => ConverterToJson[JsonArray].convert(arrayObj)
           case strObj: JsonString => ConverterToJson[JsonString].convert(strObj)
           case intObj: JsonInteger => ConverterToJson[JsonInteger].convert(intObj)
         }
-        s"""{"$key": $convertValue}"""
+        s""""$key":$convertValue"""
       }
       }.mkString(",")
+      s"{$jsonResult}"
     }
   }
 
@@ -40,7 +41,16 @@ object TypeClassJson extends App {
     )
   ))
 
+  implicit class Stringify(jsonObject: JsonObject) {
+    def stringify: String = ConverterToJson[JsonObject].convert(jsonObject)
+  }
+
+  val example = """{"name":"Bob","items":[1,"Stuff to buy"]}"""
+//  println(data.co)
+//  println(ConverterToJson[JsonObject].convert(data))
+  println(example)
   println(ConverterToJson[JsonObject].convert(data))
+  println(data.stringify)
 
 }
 
